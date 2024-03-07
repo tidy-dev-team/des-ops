e27a51e1f05d22b8d1160f8d1aceabf179d04708
Adir <slutzkis@gmail.com>
2024-03-07 17:27:58 +0200

    wp

--- desops-theme/footer.php
+++ desops-theme/footer.php
@@ -1,1 +1,11 @@
-FOOTER
+
+<?php wp_footer();?>
+
+</div>
+<div id="wrapper">
+        <iframe src="https://desops.co.il/footer.html" frameborder="0" class="full-width footer-frame"
+            onload="this.style.height=this.contentWindow.document.getElementsByTagName('body')[0].offsetHeight+'px';"></iframe>
+        <footer></footer>
+    </div>
+</body>
+</html>
--- desops-theme/front-page.php
+++ desops-theme/front-page.php
@@ -1,6 +1,8 @@
 <?php get_header();?>
 
+<h1><?php the_title();?></h1>
 
+<?php get_template_part('section', 'content');?>
 
 
 <?php get_footer();?>
